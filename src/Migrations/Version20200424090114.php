9e6f8fc4788a66735e74c4e451ea1f1dab1889cb
Sebastien Maloron <contact@smaloron.eu>
2020-04-24 11:11:03 +0200

    Création de la base de données et migration
    bin/console doctrine:database:create
    
    bin/console make:migration
    
    bin/console doctrine:migrations:migrate

---
+++ src/Migrations/Version20200424090114.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200424090114 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE book (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(150) NOT NULL, author VARCHAR(50) NOT NULL, published_at DATE NOT NULL, price NUMERIC(5, 2) NOT NULL, genre VARCHAR(50) NOT NULL, publisher VARCHAR(50) NOT NULL, text LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE book');
+    }
+}
