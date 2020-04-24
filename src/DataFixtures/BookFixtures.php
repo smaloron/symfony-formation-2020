004337d35e71728fe631c93daf6c7671a9c4ec0a
Sebastien Maloron <contact@smaloron.eu>
2020-04-24 14:33:55 +0200

    Création des fixtures pour l'entité Book avec utilisation de Faker pour obtenir des données aléatoires

---
+++ src/DataFixtures/BookFixtures.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Book;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class BookFixtures extends Fixture
+{
+
+    private  array $authorList = [
+        "Aristote", "Socrate", "Jean-Paul Sartre", "Pierre de Ronsard", "Sophie Calle", "Anne Rice"
+    ];
+
+    private array $genreList = [
+        "Poésie", "Roman", "Policier", "Essai"
+    ];
+
+    private array $publisherList = [
+        "Grasset", "Hachette", "Le Seuil", "PUF"
+    ];
+
+    private Generator $faker;
+
+    /**
+     * BookFixtures constructor.
+     */
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
+
+    public function load(ObjectManager $manager)
+    {
+
+        for($i=1; $i <= 100; $i++){
+            $book = $this->createBook();
+            $manager->persist($book);
+        }
+
+        $manager->flush();
+    }
+
+    private function createBook(){
+        $book = new Book();
+        $book->setAuthor($this->chooseOne($this->authorList))
+            ->setTitle($this->faker->catchPhrase())
+            ->setPublishedAt($this->faker->dateTimeThisCentury())
+            ->setPrice($this->faker->numberBetween(500, 90000)/100)
+            ->setGenre($this->chooseOne($this->genreList))
+            ->setPublisher($this->chooseOne($this->publisherList))
+            ->setText($this->faker->paragraph(8));
+
+
+        return $book;
+    }
+
+    private function chooseOne($collection){
+       return $collection[array_rand($collection)] ;
+    }
+}
