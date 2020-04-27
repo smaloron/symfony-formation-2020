d8cf1e6a40762a1133d0a6e8e1cb96439554d324
Sebastien Maloron <contact@smaloron.eu>
2020-04-27 11:18:57 +0200

    Traitement d'un formulaire

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -6,6 +6,7 @@ use App\Entity\Book;
 use App\Form\BookType;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -33,12 +34,25 @@ class BookController extends AbstractController
      * @Route("/new", name="book-new")
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function showForm(){
+    public function showForm(Request $request){
         //Création d'une instance de Book
         $book = new Book();
 
         //Création du formulaire
         $form = $this->createForm(BookType::class, $book);
+
+        //traitement du formulaire à partir des données de la requête
+        $form->handleRequest($request);
+
+        //Persistence de l'entité avec Doctrine
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            //Redirection vers la liste des livres
+            return $this->redirectToRoute('book-list');
+        }
 
         return $this->render("book/form.html.twig", [
             //Envoie du formulaire au modèle Twig
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Book;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +17,20 @@ class BookType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('author')
-            ->add('publishedAt')
-            ->add('price')
-            ->add('genre')
-            ->add('publisher')
-            ->add('text')
+            ->add('title', TextType::class, ['label' => 'Titre du livre', 'required' => true])
+            ->add('author', TextType::class, ['label' => 'Auteur du livre'])
+            ->add('publishedAt', DateType::class, ['label' => 'Date de publication', 'widget' => 'single_text'])
+            ->add('price', NumberType::class, ['label' => 'Prix'])
+            ->add('genre', TextType::class, ['label' => 'Genre du livre'])
+            ->add('publisher', TextType::class, ['label' => 'Editeur du livre'])
+            ->add('text', TextareaType::class, [
+                'label' => 'Résumé du livre',
+                'attr'  => ['rows' => '8']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr'  => [ 'class' => 'btn btn-block btn-primary']
+            ])
         ;
     }
 
